c7fb011761d946f662643bc30f4fc40ed55cef2d
usuario <usuario@usuario-Satellite-C645>
2015-03-13 13:37:40 -0600

    PrimeraSalida

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,12 @@
+
+package primerproyecto;
+
+public class PrimerProyecto {
+
+    
+    public static void main(String[] args) {
+       //compañeros...
+        System.out.println("Proyecto para compartir");
+    }
+    
+}
